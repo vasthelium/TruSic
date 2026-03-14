ede5a3f7dcdaf46b6e9fd4be8ff0542f5afd34d4
Syed Zameer <zameer@Syeds-MacBook-Pro.local>
2026-03-14 12:21:20 -0500

    Add mental state module and DB unit test using MagicMock

---
+++ test_db_mentalstate.py
@@ -0,0 +1,36 @@
+import pytest
+from unittest.mock import patch
+from unittest.mock import MagicMock
+from postgresdb import read_oura_daily_data
+
+# this function does
+# cursor() → execute(SQL) → fetchone() → convert tuple → dict
+
+def test_gethealthstatdb():
+    mock_conn = MagicMock() #fake connection
+    mock_cursor = MagicMock() #fake cursor
+    mock_conn.cursor.return_value = mock_cursor
+
+    mock_cursor.fetchone.return_value = (
+                            "2026-03-14",
+                            85,
+                            25200,
+                            55,
+                            60,
+                            82
+                            )
+
+    results = read_oura_daily_data(mock_conn)
+
+    fake_resultdict = {
+            "day": "2026-03-14",
+            "sleep_score": 85,
+            "total_sleep_duration": 25200,
+            "average_hrv": 55,
+            "resting_heart_rate": 60,
+            "readiness_score": 82
+        }
+
+    assert isinstance (results, dict)
+    assert results == fake_resultdict
+
